ab382fadcd3f60672f70d07c6927a4ae9c74e32c
DavideDiPietro <davide.dipietro3@gmail.com>
2022-05-09 16:01:22 +0200

    Millestone 2

--- js/main.js
+++ js/main.js
@@ -2,12 +2,12 @@ var app = new Vue({
     el: '#app',
 
     data:{
-        array: ['ciao', 'mondo']
+        disks: []
     },
-    // created(){
-    //     axios.get( 'http://localhost/php-ajax-dischi/Api/prova.php' )
-    //         .then( (res) => {
-    //             this.array = res;
-    //         })
-    // }
+    created(){
+        axios.get( 'http://localhost/php-ajax-dischi/Api/disks.php' )
+            .then( (res) => {
+                this.disks = res.data;
+            })
+    }
 })
--- partial/main.php
+++ partial/main.php
@@ -3,7 +3,10 @@
 
     <div class="container py-5">
 
+        <!-- Chiamata tramite solo php -->
+        <h1 class="text-white">Dischi utilizzando Php</h1>
         <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-5">
+
 
             <!-- Stampo tutti i dischi del databse -->
             <?php foreach( $database as $disk ) { ?>
@@ -24,9 +27,27 @@
 
         </div>
 
+        <!-- Chiamata tramite axios -->
+        <h1 class="text-white mt-5">Dischi utilizzando axios e vue</h1>
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-5">
+
+            <div v-for="(elem, index) in disks" class="col d-flex text-center g-3">
+
+                <div class="bg-card p-3">
+
+                    <img :src="elem.poster" alt="" class="img-fluid mb-3">
+                    <h5 class="text-white fw-bold mb-3">{{elem.title}}</h5>
+                    <h6 class="text-secondary">{{elem.author}}</h6>
+                    <h6 class="text-secondary">{{elem.year}}</h6>
+
+                </div>
+
+            </div>
+
+        </div>
+
+
     </div>
 
-    <!-- Prova per capire se il file.js è stato importato correttamente -->
-    <div v-for="elem in array">{{elem}}</div>
 
 </main>

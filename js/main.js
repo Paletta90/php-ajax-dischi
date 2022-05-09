a2eaadce1e5dcf1db6edf8a6fd25e1e8e3eb8332
DavideDiPietro <davide.dipietro3@gmail.com>
2022-05-09 16:39:06 +0200

    Push controllo

--- js/main.js
+++ js/main.js
@@ -2,12 +2,33 @@ var app = new Vue({
     el: '#app',
 
     data:{
-        disks: []
+        disks: [],
+        generi: ['rock', 'pop', 'jazz', 'metal'],
+        genre: ''
+    },
+    method:{
+        filterGenre: function(){
+            // axios.get( 'http://localhost/php-ajax-dischi/Api/disks.php' )
+            // .then( (res) => {
+                
+            //     this.disks = res.data;
+
+            //     this.disks.forEach(elem => {
+            //         if(elem.genre == this.genre){
+            //             this.disks.push(elem);
+            //         }
+            //     });
+
+            // })
+            console.log('ciao')
+        },
+
     },
     created(){
         axios.get( 'http://localhost/php-ajax-dischi/Api/disks.php' )
             .then( (res) => {
                 this.disks = res.data;
             })
-    }
+    },
+
 })
--- partial/header.php
+++ partial/header.php
@@ -3,9 +3,9 @@
 
             <img src="./assets/loghi/spotifyLogo.png" alt="" width="50px">
 
-            <!-- <div class="d-flex">
+            <div class="d-flex">
                 <select class="form-select form-select-sm ms-auto me-3" aria-label=".form-select-lg example"
-                    v-model="genre" @change=" $emit( 'functionSearchGenre', genre )">
+                    v-model="genre" @change="filterGenre">
 
                     <option :value="null" selected>Choose the genre</option>
                     <option v-for="(item, index) in generi" :key="index" :value="item">
@@ -14,16 +14,7 @@
 
                 </select>
 
-                <select class="form-select form-select-sm ms-auto" aria-label=".form-select-lg example" v-model="artist"
-                    @change=" $emit( 'functionSearchArtist', artist ) ">
-
-                    <option :value="null" selected>Choose the artist</option>
-                    <option v-for="(item, index) in artisti" :key="index" :value="item">
-                        {{item}}
-                    </option>
-
-                </select>
-            </div> -->
+            </div>
 
         </div>
 </header>
